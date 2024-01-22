df60610d56602f6e8b614b7c78e4a5ff1a0d7324
Vasilis Koupourtiadis <89838633+VasilisKoupourtiadis@users.noreply.github.com>
2024-01-22 14:11:39 +0100

    Add image asset collection for planets

---
+++ src/assets/nasa-image-ids.js
@@ -0,0 +1,12 @@
+const nasaImageIds = new Map([
+  ["saturn", "PIA02225"],
+  ["uranus", "PIA18182"],
+  ["neptune", "PIA00046"],
+  ["jupiter", "PIA00343"],
+  ["mercury", "PIA11245"],
+  ["venus", "PIA00271"],
+  ["earth", "PIA00122"],
+  ["mars", "PIA04591"],
+]);
+
+export default nasaImageIds;
